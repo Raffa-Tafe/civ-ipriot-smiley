c53b67287ddcc7e05398b11786454963a72ce66a
Raffa-Tafe <20126215@tafe.wa.edu.au>
2024-11-22 22:36:27 +0800

    change the code so they can use complexion as color

--- happy.py
+++ happy.py
@@ -1,21 +1,17 @@
-import time
-from blinkable import Blinkable
 from smiley import Smiley
+from blinkable import Blinkable
+import time
 
 
 class Happy(Smiley, Blinkable):
-    """
-   Provides a Smiley with a happy expression
-    """
     def __init__(self):
         super().__init__()
-
         self.draw_mouth()
         self.draw_eyes()
 
     def draw_mouth(self):
         """
-       Renders a mouth by blanking the pixels that form that object.
+        Draws the mouth feature on a happy face.
         """
         mouth = [41, 46, 50, 51, 52, 53]
         for pixel in mouth:
@@ -23,17 +19,16 @@ class Happy(Smiley, Blinkable):
 
     def draw_eyes(self, wide_open=True):
         """
-       Draws the eyes (open or closed) on the standard smiley.
-        :param wide_open (bool): eyes open or closed.
+        Draws open or closed eyes on a happy face.
+        :param wide_open: Render eyes wide open or shut.
         """
         eyes = [10, 13, 18, 21]
         for pixel in eyes:
-            self.pixels[pixel] = self.BLANK if wide_open else self.YELLOW
+            self.pixels[pixel] = self.BLANK if wide_open else self.complexion()
 
     def blink(self, delay=0.25):
         """
-       Blinks the smiley's eyes once
-        
+        Blinks the smiley's eyes once.
         :param delay: Delay between blinks (in seconds)
         """
         self.draw_eyes(wide_open=False)
